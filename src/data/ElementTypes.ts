989fe34b6d0ef3b0e8fdb349d082ff295b04ba56
Felix Neubauer <felix@neuby.de>
2018-10-06 18:32:49 +0200

    improved model: better way of handling element lists

--- src/data/ElementTypes.ts
+++ src/data/ElementTypes.ts
@@ -1,8 +1,10 @@
 import "@/data/ElementTypeModel";
-import { IElementType, ElementTypeClass, IElementTypeSimple, ElementTypeSimple, ElementTypeSimpleAutocomplete, IElementTypeComplexList } from '@/data/ElementTypeModel';
+import { IElementType, ElementTypeClass, IElementTypeSimple, ElementTypeSimple, ElementTypeSimpleAutocomplete, IElementTypeComplexList, ElementTypeComplex, ElementTypeComplexList } from '@/data/ElementTypeModel';
 
 export class ElementTypes {
 
+
+    //TODO: All all elementTypes to map and access existing elementTypes via map
 
     private static shopRoot: IElementType = ElementTypes.generateElementTypes();
     private static elementTypesSimple = {
@@ -10,12 +12,21 @@ export class ElementTypes {
         double: new ElementTypeSimple("double"),
         integer: new ElementTypeSimple("integer"),
         list_string: new ElementTypeSimple("list_string"),
-        list_item: new ElementTypeSimple("list_string"),
         //TODO: replace example names by actual lists of data names
         material: new ElementTypeSimpleAutocomplete("material", ["stone", "log"]),
         potioneffect: new ElementTypeSimpleAutocomplete("potioneffect", ["poison", "heal"]),
         enchantment: new ElementTypeSimpleAutocomplete("enchantment", ["sharpness", "unbreaking"])
     };
+
+    private static elementTypeSpecialItem = new ElementTypeComplex("item", [
+        {
+            configKey: "",
+            type: ElementTypes.elementTypesSimple.list_string
+        }
+    ]);
+
+    private static elementTypeSpecialListItem = new ElementTypeComplexList("list_item", ElementTypes.elementTypeSpecialItem, ["type:stone", "amount:1"],
+    (config: object, configKey: string) => "todo");
     
     private static elementTypeSpecialNames = ["item", "shopitemlist"];
 
