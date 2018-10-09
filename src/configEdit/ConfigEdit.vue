07650e9f1f8820af5c1b6562e298b5d328fd078c
Felix Neubauer <felix@neuby.de>
2018-10-09 21:06:38 +0200

    added YAML parser

--- src/configEdit/ConfigEdit.vue
+++ src/configEdit/ConfigEdit.vue
@@ -1,3 +1,33 @@
 <template>
-    <h1>Raw Configuration</h1>
+    <div class="ui container">
+        <div class="column">
+            <h1>Raw Configuration</h1>
+        </div>
+        <div class="column">
+            <button @click="convertToYaml()">Convert (just for test reasons)</button>
+        </div>
+        <div class="column">
+            <div class="field">
+              <textarea v-model="configText"></textarea>
+            </div>
+        </div>
+    </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue';
+import YAML from 'yamljs';
+import Component from 'vue-class-component';
+
+@Component
+export default class ConfigEdit extends Vue{
+
+    private configText: string = "";
+    private configObject: object = {};
+
+    private convertToYaml(){
+        this.configObject = YAML.parse(this.configText);
+        console.log(this.configObject);
+    }
+}
+</script>
