1b4cede07fdf0af4ece074defe27146dff35ae06
Felix Neubauer <felix@neuby.de>
2018-10-10 08:36:33 +0200

    resolved conflict with quickEdit

--- src/configEdit/ConfigEdit.vue
+++ src/configEdit/ConfigEdit.vue
@@ -8,7 +8,7 @@
         </div>
         <div class="column">
             <div class="field">
-              <textarea @click="updateSelectionSafe(false)" @select="updateSelectionSafe(false)" @keydown="updateSelectionSafe(true)" ref="configTextArea" v-model="configText"></textarea>
+              <textarea v-model="configText"></textarea>
             </div>
         </div>
     </div>
@@ -18,36 +18,13 @@
 import Vue from 'vue';
 import YAML from 'yamljs';
 import Component from 'vue-class-component';
-import _ from 'lodash';
-
 @Component
 export default class ConfigEdit extends Vue {
-
     private configText: string = "";
     private configObject: object = {};
-
-    private functionUpdateSelectionSlow = _.debounce(this.updateSelection, 1000);
-    private functionUpdateSelectionFast = _.throttle(this.updateSelection, 600);
-
     private convertToYaml() {
         this.configObject = YAML.parse(this.configText);
         console.log(this.configObject);
     }
-
-    private updateSelectionSafe(slowUpdate: boolean) {
-        if(slowUpdate){            
-            this.functionUpdateSelectionSlow.call(this);
-        }else{
-            this.functionUpdateSelectionFast.call(this);
-        }
-    }
-
-
-    private updateSelection() {
-        let element = this.$refs.configTextArea as HTMLTextAreaElement;
-        let endPosition = element.selectionEnd;
-        console.log("selection end: " + endPosition)
-    }
-
 }
 </script>
