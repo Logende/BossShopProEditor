2727df6a6c3781677d06edbd752bba47c415d4de
Felix Neubauer <felix@neuby.de>
2018-12-23 22:46:37 +0100

    small bugfix

--- src/quickEdit/properties/ShopitemlistProperty.vue
+++ src/quickEdit/properties/ShopitemlistProperty.vue
@@ -75,9 +75,11 @@ export default class ShopitemlistProperty extends Vue {
             // empty slot - create a new item
             // TODO: This might result in duplicate keys
             // if there is already an item with key "position"
+            const defaultElement = this.silType.defaultElement;
+            _.set(defaultElement, "InventoryLocation", position)
             this.$emit("input", {
                 ...this.value,
-                [position.toString()]: this.silType.defaultElement
+                [position.toString()]: defaultElement
             });
             this.$emit("change-path", [position.toString()]);
         } else {
