594e649e49d7d0e4d6fe6e949cf22e93e7e96a7c
newcat <freddy.wagner@web.de>
2019-02-12 16:11:12 +0100

    Added property description UI

---
+++ src/quickEdit/properties/PropertyDescription.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-tooltip top right :open-delay="0">
+        <v-icon slot="activator" class="ml-3" style="cursor:pointer;">help_outline</v-icon>
+        {{ description }}
+    </v-tooltip>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+
+@Component
+export default class PropertyDescription extends Vue {
+
+    @Prop({ type: String })
+    description!: string;
+
+}
+</script>
